b2a4aa60b11171d3f25dd3d68d42624e612235da
Линара Абдрашитова <Linara.Abdrashitova@nexign.com>
2025-01-17 20:43:24 +0300

    добавили curl

--- restclient/client.py
+++ restclient/client.py
@@ -3,6 +3,7 @@ from json import JSONDecodeError
 from requests import session
 import structlog
 import uuid
+import curlify
 class RestClient:
     def __init__(self, host, headers=None):
         self.host = host
@@ -54,7 +55,8 @@ class RestClient:
             data=kwargs.get('data')
         )
         rest_response = self.session.request(method=method, url=full_url, **kwargs)
-
+        curl = curlify.to_curl(rest_response.request)
+        print(curl)
         log.msg(
             event='Response',
             status_code=rest_response.status_code,
